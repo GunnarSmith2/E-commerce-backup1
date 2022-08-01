ae667e0007330c6b092f1d18f1844f111f7f359c
Gunnar Smith <gunnarsmith3@gmail.com>
2022-07-31 20:35:16 -0500

    final commit

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,4 @@
 // import important parts of sequelize library
-const { keyBy } = require("lodash");
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
 const sequelize = require("../config/connection");
